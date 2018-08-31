0fd34a16fc1ae91fffbe769352999dbdb257a8cc
Shahram Ghassemi <shahramg92@hotmail.com>
2018-08-31 12:40:33 -0500

    update

---
+++ angular-essentials/src/app/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-user',
+  template: `
+  <p>Hello!</p>
+  <p>I'm the user component</p>
+  `
+})
+export class UserComponent {}
