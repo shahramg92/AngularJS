b8188ded16a4157dd63f3936e34cd9a753f141bf
Shahram Ghassemi <shahramg92@hotmail.com>
2018-09-05 14:14:47 -0500

    update

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -13,6 +13,11 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   }
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
